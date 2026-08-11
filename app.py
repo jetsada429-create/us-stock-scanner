77b5e89f4932a2844a8d25507c4009896040115d
jetsada429-create <jetsada429@gmail.com>
2026-08-11 15:39:52 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -527,8 +527,7 @@ with tab1:
                 # บรรทัดที่ 1: ชื่อบริษัท En (ย้าย En summary ไปด้านล่าง)
                                 st.markdown(f'<p class="company-name">{res.get("longName", "N/A")}</p>', unsafe_allow_html=True)
                                 st.success(UI_LANG_MAP['success_stock_found_single'].format(ticker=single_ticker) + f' | ข้อมูล ณ วันที่: {res["Date"]}')
-
-               # แบ่ง Col หลัก: ซ้าย (ข้อมูล) | ขวา (กราฟ)
+             # แบ่ง Col หลัก: ซ้าย (ข้อมูล) | ขวา (กราฟ)
                 col_info, col_chart = st.columns([1, 2])
                 
                 with col_chart:
@@ -546,7 +545,7 @@ with tab1:
                     
                     # *** แก้ไข: ปรับย่อหน้าบรรทัดนี้ให้ตรงกับคอมเมนต์ด้านบน ***
                     st.info(f"🤖 {UI_LANG_MAP['placeholder_pattern_match']}")
-
+              
                 with col_info:
                     st.markdown(UI_LANG_MAP['analysis_title'])
                     
